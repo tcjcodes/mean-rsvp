65a91aa57c752f4dc7059d5ecc43746cc29e2e8a
Trina <tcjcastillo@gmail.com>
2017-07-28 22:48:46 -0700

    project setup + dependencies for server

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'tcjcodes.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://admin:startup1@ds127783.mlab.com:27783/tc-mean-rsvp'
+};
